2753497b680f3018f4cb4439fe014a25dd387b83
Miles Holt <contact@milesholt.co.uk>
2024-04-11 14:18:35 +0000

    Added hero slider to shop page

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import {
+  ViewChild,
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  ElementRef,
+} from '@angular/core';
 import { CoreService } from '../../services/core.service';
 import { Library } from '../../app.library';
 import { Router, ActivatedRoute } from '@angular/router';
 //import { ProductComponent } from './product/product.component';
+
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-shop',
@@ -13,6 +22,24 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ShopComponent implements OnInit {
   products: any = [];
   filterProducts: any = [];
+  filterCategory: string = 'all';
+
+  heroSlides: any = {
+    Mushroom: {
+      product: 'Mushroom',
+      slides: [
+        {
+          title: 'Fungi',
+          description: 'Discover the power of mushrooms',
+          //image:'https://cdn.shopify.com/s/files/1/0815/3694/2417/products/1707134754929-generated-label-image-0.jpg?v=170802151',
+          image:
+            'https://www.pexels.com/photo/close-up-photo-of-white-mushrooms-1643416/',
+        },
+      ],
+    },
+  };
+
+  @ViewChild('mainSwiper', { static: false }) mainSwiper!: ElementRef;
 
   constructor(
     public service: CoreService,
@@ -27,6 +54,8 @@ export class ShopComponent implements OnInit {
     this.test();
     this.checkReturn();
   }
+
+  ngAfterViewInit() {}
 
   async checkReturn() {
     if (this.router.url.indexOf('?return') > -1) {
@@ -43,11 +72,17 @@ export class ShopComponent implements OnInit {
   async handleSearchCallback(searchData: any) {
     const results = searchData.results;
     const keyword = searchData.keyword;
+    this.filterCategory = searchData.keyword;
 
     console.log(results);
     if (keyword == '') this.test();
     this.filterProducts = results;
     this.cdr.detectChanges();
+    setTimeout(() => {
+      if (this.mainSwiper) {
+        this.checkSlider();
+      }
+    });
   }
 
   async handleFilterCallback(filterData: any) {
@@ -64,6 +99,46 @@ export class ShopComponent implements OnInit {
       console.log(products);
       this.products = products;
       this.filterProducts = products;
+      setTimeout(() => {
+        if (this.mainSwiper) {
+          this.checkSlider();
+        }
+      });
     });
+  }
+
+  async checkSlider() {
+    if (Object.keys(this.heroSlides).includes(this.filterCategory)) {
+      //alert('here');
+      //await this.iniSlider();
+      await this.iniSlider();
+      //
+    }
+  }
+
+  async iniSlider() {
+    // swiper element
+    //const swiperEl = <any>document.querySelector('.main-swiper');
+    const swiperEl = this.mainSwiper.nativeElement;
+    // swiper parameters
+    const swiperParams = {
+      slidesPerView: 1,
+      navigation: false,
+      /*pagination: {
+        clickable: true,
+      },*/
+      /*thumbs: {
+        swiper: '.thumbs-swiper',
+      },*/
+      on: {
+        init() {},
+      },
+    };
+
+    // now we need to assign all parameters to Swiper element
+    Object.assign(swiperEl, swiperParams);
+
+    // and now initialize it
+    swiperEl.initialize();
   }
 }
--- src/app/pages/shop/shop.module.ts
+++ src/app/pages/shop/shop.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { ShopComponent } from './shop.component';
 
 import { SearchShopModule } from './components/search/search.shop.module';
 import { FilterShopModule } from './components/filter/filter.shop.module';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     IonicModule,
     CommonModule,
